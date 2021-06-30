5ff8929b71d96041017c5966dafb4a7479feac73
Gorvarydar <bothisav@gmail.com>
2021-06-30 22:58:44 +0300

    '3'

---
+++ scripts/start.js
@@ -0,0 +1,19 @@
+const Hapi = require('@hapi/hapi');
+
+const port = process.env.PORT || 3000;
+
+const init = async () => {
+    const server = Hapi.server({
+        port,
+    })
+    await server.register(require('@hapi/inert'));
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+
+}
+process.on('unhandledRejection',(err)=>{
+    console.log(err);
+    process.exit(1);
+})
+init();
